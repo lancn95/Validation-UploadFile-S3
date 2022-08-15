b5588aa325a94bbbd1b9574ed726510b8c3bef00
lancn95 <catngoclan95@gmail.com>
2022-08-15 10:09:22 +0700

    add swagger

--- src/main/java/com/amigos/awsuploadimage/api/UserProfileApi.java
+++ src/main/java/com/amigos/awsuploadimage/api/UserProfileApi.java
@@ -4,6 +4,8 @@ import com.amigos.awsuploadimage.profile.UserProfile;
 import com.amigos.awsuploadimage.request.UserProfileCreateRequest;
 import com.amigos.awsuploadimage.request.UserProfileUpdateRequest;
 import com.amigos.awsuploadimage.service.UserProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/user-profile")
 @CrossOrigin("*")
+@Tag(name = "user-profile-well-define-APIs")
 public class UserProfileApi {
 
     private final UserProfileService userProfileService;
@@ -27,11 +30,13 @@ public class UserProfileApi {
     }
 
     @GetMapping
+    @Operation(summary = "get all users")
     public List<UserProfile> getUserProfiles() {
         return userProfileService.findAllUserProfile();
     }
 
     @PostMapping()
+    @Operation(summary = "create new user")
     public ResponseEntity<UserProfile> createNewUserProfile(@RequestBody @Valid UserProfileCreateRequest userProfileRequest,
                                                             BindingResult bindingResult)
             throws MethodArgumentNotValidException {
@@ -41,16 +46,18 @@ public class UserProfileApi {
         return new ResponseEntity<>(userProfileService.createNewUserProfile(userProfileRequest), HttpStatus.CREATED);
     }
 
-    @PostMapping(path = "{userProfileId}/image/download",
+    @PostMapping(path = "{userProfileId}/image/upload",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "upload user image ")
     public void uploadUserProfileImage(@PathVariable("userProfileId") Long id,
                                        @RequestParam("file") MultipartFile file) {
         userProfileService.uploadUserProfileImage(id, file);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "update user profile ")
     public ResponseEntity<UserProfile> update(@PathVariable("id") Long id,
                                               @RequestBody @Valid UserProfileUpdateRequest request){
         userProfileService.update(id, request);
