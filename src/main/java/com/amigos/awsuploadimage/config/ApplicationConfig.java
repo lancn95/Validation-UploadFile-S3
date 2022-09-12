620477b357054a285c6e40cf2a823771eb789ccf
lancn95 <catngoclan95@gmail.com>
2022-09-12 09:25:44 +0700

    init page admin

--- src/main/java/com/amigos/awsuploadimage/config/ApplicationConfig.java
+++ src/main/java/com/amigos/awsuploadimage/config/ApplicationConfig.java
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 @Configuration
 public class ApplicationConfig extends WebMvcConfigurationSupport {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"};
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/**")
-                .addResourceLocations("/static/files");
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
--- src/main/java/com/amigos/awsuploadimage/controller/AdminController.java
+++ src/main/java/com/amigos/awsuploadimage/controller/AdminController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/card")
-public class HelloWorld {
-
+@RequestMapping("/admin")
+public class AdminController {
     @GetMapping("")
-    public String helloWorld(){
-        return "pages/card/card";
+    public String getAdminPage(){
+        return "pages/admin/admin";
     }
 }
